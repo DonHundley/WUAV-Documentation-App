ffc375bd4330fc5cce987d14b0f098d2db1bbef7
DonHundley <112941023+DonHundley@users.noreply.github.com>
2023-04-25 09:03:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application{
+
+    private Stage primaryStage;
+    private Stage prevStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
